073b47524dc65d67483acad1632c67b8b0550557
Kirty91 <kirty2991@example.com>
2019-10-15 19:30:21 -0700

    Completed lesson 20

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,22 @@
+let age=50;
+let firstName="Kirty";
+let courseCompleted=true;
+let finalScore=null;
+let winningNumbers;
+
+console.log("age:",age);
+console.log("First Name:",firstName);
+console.log("Course Completed:",courseCompleted);
+console.log("Final Score:",finalScore);
+console.log("Winning Numbers:",winningNumbers);
+// Conversions
+console.log("age to String:",String(age));
+console.log("age to Boolean:",Boolean(age));
+console.log("First Name to Number:",Number(firstName));
+console.log("Course Completed to Number:",Number(courseCompleted));
+console.log("Final Score to Boolean:",Boolean(finalScore));
+console.log("Final Score to Number:",Number(finalScore));
+console.log("Final Score to String:",String(finalScore));
+console.log("Winning Numbers to String:",String(winningNumbers));
+console.log("Winning Numbers to Boolean:",Boolean(winningNumbers));
+console.log("Winning Numbers to Number:",Number(winningNumbers));
