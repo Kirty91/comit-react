9737ee3c465ac3e8694d6f72e78d1d6254f65ec0
Kirty91 <kirty2991@example.com>
2019-10-15 18:43:52 -0700

    Completed lesson 19

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,19 @@
+const monthOfBirth="October";
+const firstName="Kirty";
+const lastName="Sharma";
+
+let city;
+let age;
+let nextCity;
+
+city="Saskatoon";
+age=25;
+nextCity=city;
+
+console.log('Month Of Birth: ', monthOfBirth);
+console.log('First Name: ', firstName);
+console.log('Last Name: ', lastName);
+console.log("Full Name:",`${firstName} ${lastName}`);
+console.log('City: ', city);
+console.log('Age: ', age);
+console.log('Next City: ', nextCity);
