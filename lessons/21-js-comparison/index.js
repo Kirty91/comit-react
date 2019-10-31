f90d704bc60c05181220763cc4a15660c961508e
LovedeepKaur42 <kaurlovedeep108@gmail.com>
2019-10-31 15:27:12 -0400

    completed lesson 21

---
+++ lessons/21-js-comparison/index.js
@@ -0,0 +1,5 @@
+alert("Enter two numbers to see if the first is greater than the second");
+let firstNum = prompt('enter the first number', '');
+let secondNum = prompt('enter the second number', '');
+let result = confirm(firstNum>secondNum);
+//alert(result);
